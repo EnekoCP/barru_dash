9c97525762b1bbcbb50dd91fe1326c45a9f0e089
Eneko <hello@lup.es>
2024-01-08 22:00:54 +0100

    oled

--- dash_dev/dash.py
+++ dash_dev/dash.py
@@ -11,8 +11,66 @@ from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
 # Inicializamos pygame
 pygame.init()
+
+def oled_test():
+    # Define the Reset Pin
+    oled_reset = digitalio.DigitalInOut(board.D4)
+
+    # Change these
+    # to the right size for your display!
+    WIDTH = 128
+    HEIGHT = 32  # Change to 64 if needed
+    BORDER = 5
+
+    # Use for I2C.
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+    oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+    # Clear display.
+    oled.fill(0)
+    oled.show()
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    image = Image.new("1", (oled.width, oled.height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a white background
+    draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+    # Draw a smaller inner rectangle
+    draw.rectangle(
+        (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+        outline=0,
+        fill=0,
+    )
+
+    # Load default font.
+    font = ImageFont.load_default()
+
+    # Draw Some Text
+    text = "Hello World!"
+    (font_width, font_height) = font.getsize(text)
+    draw.text(
+        (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+        text,
+        font=font,
+        fill=255,
+    )
+
+    # Display image
+    oled.image(image)
+    oled.show()
 
 def reproducir_sonido(cambio_marcha):
     if cambio_marcha:
@@ -68,6 +126,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
+        oled_test()
         demo_tablero_coche(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
     except KeyboardInterrupt:
         pass

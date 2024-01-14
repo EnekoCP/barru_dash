96ab9630e1f28b3869346cd78f55e1a441b8e9b8
Eneko <hello@lup.es>
2024-01-14 21:53:43 +0100

    oled

--- dash_dev/dash.py
+++ dash_dev/dash.py
@@ -137,7 +137,7 @@ def init_leds_oled():
 
 
 def init_oled():
-    draw.text((5, 5), "SUBARU IMPREZA", font=font, fill=255)
+    draw.text((12, 5), "SUBARU IMPREZA", font=font, fill=255)
     draw.text((5, 17), "SimHub by CHUME", font=font, fill=255)
 
     # Display updated image
